48fe742fade44a2bd2d6d0be377a42a886203228
Alexey <leshka.lapunov@gmail.com>
2019-11-07 00:29:25 +0300

    start pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
+import classnames from 'classnames';
+
 import styles from './Pagination.module.scss';
 
 class Pagination extends React.Component {
   render() {
-    return <div className={styles.pagination}>Pagination</div>;
+    return (
+      <div className={styles.pagination}>
+        <button
+          type="button"
+          className={classnames(styles.button, styles.prev)}
+        >
+          P
+        </button>
+        <button className={styles.link}>1</button>
+        <span className={styles.dots}>...</span>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.link}>1</button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.link}>2</button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.link}>3</button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.link}>4</button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.link}>5</button>
+          </li>
+        </ul>
+        <span className={styles.dots}>...</span>
+        <button className={styles.link}>20</button>
+        <button
+          type="button"
+          className={classnames(styles.button, styles.next)}
+        >
+          N
+        </button>
+      </div>
+    );
   }
 }
 
