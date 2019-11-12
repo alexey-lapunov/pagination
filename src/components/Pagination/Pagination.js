fbb5a66546033ca26a1093a8fc9c0ed875d24b1b
Alexey Lapunov <alexey.lapunov@ontravelsolutions.com>
2019-11-12 14:46:31 +0300

    class component replaced by functional component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,97 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classnames from "classnames";
 
 import styles from "./Pagination.module.scss";
 
-class Pagination extends React.Component {
-    state = {
-        activePage: this.props.activePage || 1,
-        arrayPages: Array.from(Array(this.props.pageNumber || 0), (item, i) => i + 1)
+const Pagination = ({activePage: activePageProps = 1, pageNumber = 0, pageSize = 5}) =>  {
+    const [ activePage, setActivePage ] = useState(activePageProps);
+    const [ arrayPages ] = useState(Array.from(Array(pageNumber), (item, i) => i + 1));
+
+    const nextPage = () => {
+        if (activePage !== arrayPages.length) setActivePage(activePage + 1);
     };
 
-    nextPage = () => {
-        const { activePage, arrayPages } = this.state;
-        if (activePage !== arrayPages.length)
-            this.setState({ activePage: activePage + 1 });
+    const prevPage = () => {
+        if (activePage !== 1) setActivePage(activePage - 1);
     };
 
-    prevPage = () => {
-        const { activePage } = this.state;
-        if (activePage !== 1) this.setState({ activePage: activePage - 1 });
-    };
+    const goToPage = index => setActivePage(index);
 
-    goToPage = index => this.setState({ activePage: index });
+    const firstPage = () => setActivePage(1);
+    const lastPage = () => setActivePage(arrayPages.length);
 
-    firstPage = () => this.setState({ activePage: 1 });
-    lastPage = () => this.setState({ activePage: this.state.arrayPages.length });
+    const activePageIndex = activePage - 1;
+    const firstIndexDisplayedLink = arrayPages[activePageIndex - 3]
+        ? (
+            arrayPages[activePageIndex + 3]
+                ? activePageIndex - 2
+                : arrayPages.length - 5
+        )
+        : 0;
+    const lastIndexDisplayedLink = firstIndexDisplayedLink
+        ? (
+            firstIndexDisplayedLink <= arrayPages.length - 6
+                ? activePageIndex + 2
+                : arrayPages.length - 1
+        )
+        : pageSize - 1;
 
-    render() {
-        const { activePage, arrayPages } = this.state;
-        const { pageSize } = this.props;
-        const activePageIndex = activePage - 1;
-        const firstIndexDisplayedLink = arrayPages[activePageIndex - 3]
-            ? (
-                arrayPages[activePageIndex + 3]
-                    ? activePageIndex - 2
-                    : arrayPages.length - 5
-            )
-            : 0;
-        const lastIndexDisplayedLink = firstIndexDisplayedLink
-            ? (
-                firstIndexDisplayedLink <= arrayPages.length - 6
-                    ? activePageIndex + 2
-                    : arrayPages.length - 1
-            )
-            : pageSize - 1;
-        console.log(firstIndexDisplayedLink, lastIndexDisplayedLink);
-        console.log(arrayPages[firstIndexDisplayedLink], arrayPages[lastIndexDisplayedLink])
-
-        return (
-            <div className={styles.pagination}>
-                <button
-                    type="button"
-                    onClick={this.prevPage}
-                    className={classnames(styles.button, styles.prev)}
-                >
-                    P
-                </button>
-                <button className={styles.link} onClick={this.firstPage}>
+    return (
+        <div className={styles.pagination}>
+            <button
+                type="button"
+                onClick={prevPage}
+                className={classnames(styles.button, styles.prev)}
+            >
+                P
+            </button>
+            {!!firstIndexDisplayedLink && (<>
+                <button className={styles.link} onClick={firstPage}>
                     1
                 </button>
                 <span className={styles.dots}>...</span>
-                <ul className={styles.list}>
-                    {arrayPages.map((item, i) => {
-                        if (i >= firstIndexDisplayedLink && i <= lastIndexDisplayedLink) {
-                            return (
-                                <li className={styles.item} key={i}>
-                                    <button
-                                        className={classnames(styles.link, {
-                                            [styles.active]: activePage === item
-                                        })}
-                                        onClick={() => this.goToPage(item)}
-                                    >
-                                        {item}
-                                    </button>
-                                </li>
-                            );
-                        }
-                    })}
-                </ul>
+            </>)}
+
+            <ul className={styles.list}>
+                {arrayPages.map((item, i) => {
+                    if (i >= firstIndexDisplayedLink && i <= lastIndexDisplayedLink) {
+                        return (
+                            <li className={styles.item} key={i}>
+                                <button
+                                    className={classnames(styles.link, {
+                                        [styles.active]: activePage === item
+                                    })}
+                                    onClick={() => goToPage(item)}
+                                >
+                                    {item}
+                                </button>
+                            </li>
+                        );
+                    }
+                    return null;
+                })}
+            </ul>
+            {lastIndexDisplayedLink !== arrayPages.length - 1 && (<>
                 <span className={styles.dots}>...</span>
-                <button className={styles.link} onClick={this.lastPage}>
+                <button className={styles.link} onClick={lastPage}>
                     {arrayPages.length}
                 </button>
-                <button
-                    type="button"
-                    onClick={this.nextPage}
-                    className={classnames(styles.button, styles.next)}
-                >
-                    N
-                </button>
-            </div>
-        );
-    }
-}
+            </>)}
+            <button
+                type="button"
+                onClick={nextPage}
+                className={classnames(styles.button, styles.next)}
+            >
+                N
+            </button>
+        </div>
+    );
+};
 
 export { Pagination };
